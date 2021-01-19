3dac49963595f011f0b0d58f2c2a833c1c3c8309
bswan0002 <bswan0002@gmail.com>
2021-01-19 10:37:52 -0800

    passing

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(uName, uEmail) {
+  let userObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: `${uName}`,
+      email: `${uEmail}`,
+    }),
+  };
+  return fetch("http://localhost:3000/users", userObj)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      let newId = document.createElement("p");
+      newId.innerHTML = object.id;
+      document.querySelector("h2").appendChild(newId);
+      console.log(object);
+    })
+    .catch(function (error) {
+      let errorMsg = document.createElement("p");
+      errorMsg.innerHTML = error.message;
+      document.querySelector("h2").appendChild(errorMsg);
+    });
+}
